3a930bdfe8d97098700d19473822d42532f49f30
xakeplusplus <87079882+xakeplusplus@users.noreply.github.com>
2022-07-17 17:10:15 +0400

    Update Discord.java

--- src/main/java/me/xakeplusplus/spg/util/Discord.java
+++ src/main/java/me/xakeplusplus/spg/util/Discord.java
@@ -8,31 +8,30 @@ import me.xakeplusplus.spg.SpaghettiClient;
 import net.minecraft.client.Minecraft;
 
 public class Discord {
-	private static final DiscordRichPresence discordRichPresence = new DiscordRichPresence();
-    private static final DiscordRPC discordRPC = DiscordRPC.INSTANCE;
-    static String discordID = "882321475284111450";
+    	private static final DiscordRichPresence discordRichPresence = new DiscordRichPresence();
+    	private static final DiscordRPC discordRPC = DiscordRPC.INSTANCE;
+    	static String discordID = "882321475284111450";
 	
 	public static void startrpc() {
 		DiscordEventHandlers eventHandlers = new DiscordEventHandlers();
-        eventHandlers.disconnected = ((var1, var2) -> System.out.println("Discord RPC disconnected, var1: " + var1 + ", var2: " + var2));
 
-        discordRPC.Discord_Initialize(discordID, eventHandlers, true, null);
+		discordRPC.Discord_Initialize(discordID, eventHandlers, true, null);
 
-        
-        
-        discordRichPresence.startTimestamp = System.currentTimeMillis() / 1000L;
-        discordRichPresence.largeImageKey = "th-3201710860";
-        discordRichPresence.largeImageText = "https://github.com/xakeplusplus/SpaghettiClient";
-        if(Minecraft.getMinecraft().player != null && Minecraft.getMinecraft().world != null) {
-        	discordRichPresence.details = "SpaghettiClient v" + Reference.VERSION;
-        }
-        else {
-        	discordRichPresence.details = "In the menu.";
-        }
-        
-        discordRichPresence.state = Minecraft.getMinecraft().player.getName();
-        discordRPC.Discord_UpdatePresence(discordRichPresence);
-        SpaghettiClient.log.info("discord rpc started");
+
+
+		discordRichPresence.startTimestamp = System.currentTimeMillis() / 1000L;
+		discordRichPresence.largeImageKey = "th-3201710860";
+		discordRichPresence.largeImageText = "https://github.com/xakeplusplus/SpaghettiClient";
+		if(Minecraft.getMinecraft().player != null && Minecraft.getMinecraft().world != null) {
+			discordRichPresence.details = "SpaghettiClient v" + Reference.VERSION;
+		}
+		else {
+			discordRichPresence.details = "In the menu.";
+		}
+
+		discordRichPresence.state = Minecraft.getMinecraft().player.getName();
+		discordRPC.Discord_UpdatePresence(discordRichPresence);
+		SpaghettiClient.log.info("discord rpc started");
 	}
 	
 	public static void stoprpc() {
